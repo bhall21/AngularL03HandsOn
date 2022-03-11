eaf5f29dcd15cadfe408cd7be031ce05b8f8dacb
Brad Hall <bhall228@gmail.com>
2022-03-11 01:18:46 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularL03';
+  title = 'Mr. Bigglesworth';
+  city = "Susanville";
+  TagLine = "Seeking the Holy Spirit must come first";
+  AboutMe = "I am loving husband that puts Christ first in my marriage. I know that all things are possible in Christ. I am living proof that God still does miricles today, just as he did in the Bible and He will continue to do them in the future! ";
 }
